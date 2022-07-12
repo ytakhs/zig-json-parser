94374cb18a52ae61b93a56897779753ce9e77e2b
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-12 22:26:52 +0900

    null, boolean

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,91 @@
 const std = @import("std");
 const testing = std.testing;
 
+const Error = error{
+    SyntaxError,
+};
+
+const Value = union(enum) {
+    Null,
+    Boolean: bool,
+};
+
 pub const Parser = struct {
     alloc: std.mem.Allocator,
     buf: []const u8,
+    cur: usize,
+
+    const Self = @This();
 
     pub fn init(alloc: std.mem.Allocator, buf: []const u8) Parser {
-        return .{ .alloc = alloc, .buf = buf };
+        return .{ .alloc = alloc, .buf = buf, .cur = 0 };
     }
 
-    pub fn parse(this: *Parser) !void {
-        std.debug.print("{s}\n", .{this.buf});
+    pub fn deinit(self: *Self) void {
+        self.alloc.deinit();
+    }
+
+    pub fn parse(self: *Parser) !Value {
+        self.skipWhitespace();
+
+        const p = self.buf[0];
+        return switch (p) {
+            'n' => Value{ .Null = try self.parseNull() },
+            't', 'f' => Value{ .Boolean = try self.parseBoolean() },
+            else => Error.SyntaxError,
+        };
+    }
+
+    fn parseNull(self: *Parser) Error!void {
+        const v = self.peek(4) orelse return Error.SyntaxError;
+
+        if (std.mem.eql(u8, v, "null")) {
+            return;
+        }
+
+        return Error.SyntaxError;
+    }
+
+    fn parseBoolean(self: *Parser) Error!bool {
+        switch (self.buf[self.cur]) {
+            't' => {
+                const v = self.peek(4) orelse return Error.SyntaxError;
+
+                return std.mem.eql(u8, v, "true");
+            },
+            'f' => {
+                const v = self.peek(5) orelse return Error.SyntaxError;
+
+                return !std.mem.eql(u8, v, "false");
+            },
+            else => return Error.SyntaxError,
+        }
+    }
+
+    pub fn peek(self: *Self, n: usize) ?[]const u8 {
+        if (self.buf.len < n) {
+            return null;
+        }
+
+        return self.buf[0..n];
+    }
+
+    fn skipWhitespace(self: *Self) void {
+        while (true) {
+            switch (self.buf[self.cur]) {
+                ' ', '\n' => {
+                    self.cur += 1;
+                },
+                else => break,
+            }
+        }
     }
 };
 
 test {
     const alloc = std.testing.allocator;
 
-    var parser = Parser.init(alloc, "{}");
-    _ = try parser.parse();
+    try std.testing.expectEqual(Value.Null, try Parser.init(alloc, "null").parse());
+    try std.testing.expectEqual(Value{ .Boolean = true }, try Parser.init(alloc, "true").parse());
+    try std.testing.expectEqual(Value{ .Boolean = false }, try Parser.init(alloc, "false").parse());
 }
