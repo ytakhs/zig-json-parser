27e3e73217b1f7e9386555f39c1943599d67f7b2
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-12 22:40:18 +0900

    string

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ const Error = error{
 const Value = union(enum) {
     Null,
     Boolean: bool,
+    String: []const u8,
 };
 
 pub const Parser = struct {
@@ -32,6 +33,7 @@ pub const Parser = struct {
         return switch (p) {
             'n' => Value{ .Null = try self.parseNull() },
             't', 'f' => Value{ .Boolean = try self.parseBoolean() },
+            '"' => Value{ .String = try self.parseString() },
             else => Error.SyntaxError,
         };
     }
@@ -62,6 +64,22 @@ pub const Parser = struct {
         }
     }
 
+    fn parseString(self: *Parser) Error![]const u8 {
+        self.cur += 1;
+        const start = self.cur;
+        var end = self.cur;
+
+        while (self.buf[end] != '"') : (end += 1) {
+            if (end >= self.buf.len) {
+                return Error.SyntaxError;
+            }
+        }
+
+        self.cur = end;
+
+        return self.buf[start..end];
+    }
+
     pub fn peek(self: *Self, n: usize) ?[]const u8 {
         if (self.buf.len < n) {
             return null;
@@ -88,4 +106,5 @@ test {
     try std.testing.expectEqual(Value.Null, try Parser.init(alloc, "null").parse());
     try std.testing.expectEqual(Value{ .Boolean = true }, try Parser.init(alloc, "true").parse());
     try std.testing.expectEqual(Value{ .Boolean = false }, try Parser.init(alloc, "false").parse());
+    try std.testing.expect(std.mem.eql(u8, "foo", (try Parser.init(alloc, "\"foo\"").parse()).String));
 }
